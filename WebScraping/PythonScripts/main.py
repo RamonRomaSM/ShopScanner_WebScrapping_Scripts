9d8a5366ce756d3e267427b8c9c2ae604df118d3
RamonRomaSM <125303729+RamonRomaSM@users.noreply.github.com>
2024-11-05 20:46:53 +0100

    Estructura cambiada

--- WebScraping/PythonScripts/main.py
+++
@@ -1,8 +0,0 @@
-import AlcampoScraper
-import AccesoBdd
-import MercadonaScraper
-
-AccesoBdd.borrarBdd()
-AlcampoScraper.startScraping()
-MercadonaScraper.startScraping()
-print('[SISTEMA] Base de datos actualizada')
